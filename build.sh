f4f34469b0433ed803c124067593005945978886
lchouville <luka.chouville@laposte.net>
2024-06-24 10:58:32 +0200

    Build Push

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
+# Install dependencies
 npm install
+# Push the code in develop
+git add *
+git commit -m "Build Push"
+git push
+#  Build libraries
 npm run build
-
+# Push Lib in release   
 git add lib/*
 git add README.md
 git add release-notes.txt
+git add .gitignore
 git commit -m "Release version"
 git push origin release
