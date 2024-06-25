bfd2a003ce62590acdc3d46afc03038bdb790ee1
lchouville <luka.chouville@laposte.net>
2024-06-25 10:12:21 +0200

    release/valided-update

--- valided-release.sh
+++ valided-release.sh
@@ -6,6 +6,7 @@ git pull origin main
 
 # Copy the latest README.md, and release-notes.txt from develop
 git checkout release -- README.md
+git checkout release -- lib/*
 git checkout release -- release-notes.txt
 
 # Add the built libraries and specific files to the release branch
