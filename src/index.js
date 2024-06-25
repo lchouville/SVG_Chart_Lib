89e1d2c81efc0568f43a5d20da00cf4ae71af301
lchouville <luka.chouville@laposte.net>
2024-06-25 14:15:03 +0200

    Build Push

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import { NewElement, newGraph } from './function/graph.js';
+// import grah
+import {svg_Progressbar_s} from './graph/bar/progressBar.js';
+import {svg_ColumnChart_s} from './graph/bar/columnChart.js';
+import {svg_stackedbar100_s} from './graph/bar/stackedBar.js';
+import {svg_PieChart_s} from './graph/bar/pieChart.js';
+import {svg_ProgressCircle_s} from './graph/bar/progressCircle.js';
+import {svg_radarChart_s} from './graph/bar/radarChart.js';
+
+
+export { 
+    NewElement, 
+    newGraph,
+    svg_Progressbar_s,
+    svg_ColumnChart_s,
+    svg_stackedbar100_s,
+    svg_PieChart_s,
+    svg_ProgressCircle_s,
+    svg_radarChart_s,
+};
