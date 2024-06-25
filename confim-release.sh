184362082674e18b830bf942bcd16654ce34030e
lchouville <luka.chouville@laposte.net>
2024-06-25 10:04:51 +0200

    release/confirm.sh

---
+++ confim-release.sh
@@ -0,0 +1,14 @@
+# Checkout the release branch
+git checkout main
+
+# Pull the latest changes from the release branch
+git pull origin main
+
+# Copy the latest README.md, and release-notes.txt from develop
+git checkout release -- README.md
+git checkout release -- release-notes.txt
+
+# Add the built libraries and specific files to the release branch
+git add lib/*
+git add README.md
+git add release-notes.txt
